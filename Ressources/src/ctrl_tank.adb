97f2ead85d7fc5cb2ceceae4c81c1c3d23aec8a6
Otávio de Meira Lima <33879489+otavioml@users.noreply.github.com>
2023-06-15 17:16:54 +0200

    Fixing conversions of range types

--- Ressources/src/ctrl_tank.adb
+++ Ressources/src/ctrl_tank.adb
@@ -1,21 +1,35 @@
-with Tank, Tk_Periph.Leds, Tk_Periph.Ultrasonic, Tk_Periph.Motors,
-   Tk_Periph.Tracking;
-use Tank, Tk_Periph.Leds, Tk_Periph.Motors, Tk_Periph.Tracking,
-   Tk_Periph.Ultrasonic;
+with
+    Tank,
+    Tk_Periph.Leds,
+    Tk_Periph.Ultrasonic,
+    Tk_Periph.Motors,
+    Tk_Periph.Tracking,
+    Ada.Text_IO,
+    Pcmc_Simple_Frame,
+    Pcmc_Header,
+    Pcmc_Notes,
+    Pcmc_Simple_Frame.Io,
+    GNAT.Sockets;
+use
+    Tank,
+    Tk_Periph.Leds,
+    Tk_Periph.Ultrasonic,
+    Tk_Periph.Motors,
+    Tk_Periph.Tracking,
+    Ada.Text_IO,
+    Pcmc_Simple_Frame,
+    Pcmc_Header,
+    Pcmc_Notes,
+    Pcmc_Simple_Frame.Io,
+    GNAT.Sockets;
 with TbN;
 with Ada.Real_Time; use Ada.Real_Time;
-with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Ctrl_Tank is
     Le_Tank : T_Tank;
-    Piste   : T_Tracking;
-    Pow     : constant T_Power := 40;
-    package Danger is
-        type T_Danger is (Aucun, Pause, Stop);
-    end Danger;
-    use Danger;
-    package TbN_Danger is new TbN (T_Danger);
-    Arret : TbN_Danger.T_TbN;
+    package TbN_Puissance is new TbN (Pcmc_Header.T_Power);
+    use TbN_Puissance;
+    Puissance : T_TbN;
 begin
     Le_Tank.Init ("Mon Tank 2");
     Le_Tank.Add_Tracking_Simu;
@@ -25,75 +39,96 @@ begin
     Le_Tank.Color_Leds (Green);
 
     declare
-        task Detecter_Afficher;
-        task body Detecter_Afficher is
-            periode : constant Time_Span := Ada.Real_Time.Milliseconds (20);
-            Echeance : Time;
-            distance : T_Distance;
-            garder_loop : Boolean := True;
+        task Recv_Trame;
+        task body Recv_Trame is
+            Socket  : Socket_Type;
+            Client  : Socket_Type;
+            Addr    : Sock_Addr_Type;
+            Cmpt    : Integer := 0;
         begin
-            while garder_loop loop
-                Echeance := Clock + periode;
-                distance := Le_Tank.Get_Ultrasonic_Distance;
-                if distance > 50 then
-                    Le_Tank.Color_Leds (Green);
-                    Arret.Ecrire (Aucun);
-                elsif distance > 30 and distance < 50 then
-                    Le_Tank.Color_Leds (Yellow);
-                    Arret.Ecrire (Aucun);
-                elsif distance > 10 and distance < 30 then
-                    Le_Tank.Color_Leds (Magenta);
-                    Arret.Ecrire (Aucun);
-                else
-                    Le_Tank.Color_Leds (Red);
-                    if distance > 5 then
-                        Arret.Ecrire (Pause);
-                    else
-                        Arret.Ecrire (Stop);
-                        garder_loop := False;
-                    end if;
-                end if;
-                delay until Echeance;
-            end loop;
-        end Detecter_Afficher;
+            Create_Socket (Socket, Family_Inet, Socket_Stream);
+            Addr.Addr := Any_Inet_Addr;
+            Addr.Port := Port_Type'Value ("7777");
+            Bind_Socket (Socket, Addr);
+            Put_Line ("Listening on port" & Port_Type'Image (Addr.Port));
+            Listen_Socket (Socket);
+            Accept_Socket (Server => Socket,
+                Socket => Client, Address => Addr);
+            Put_Line ("Connecte !!");
+            Ctrl_C_Handle;
+            Boucle_Principale : loop
+                declare
+                    Frame : constant T_Pcmc_Simple_Frame :=
+                        Receive (Client);
+                    Header : constant T_Pcmc_Frame_Header :=
+                        Get_Header (Frame);
+                    Pui : constant Pcmc_Header.T_Power := Get_Power (Header);
+                begin
+                    Cmpt := Cmpt + 1;
+                    Put_Line ("===> Trame " & Positive'Image (Cmpt) &
+                        " :");
+                    Put ("Receiving the power " &
+                        Pcmc_Header.T_Power'Image (Pui));
+                    New_Line;
+                    Puissance.Ecrire (Pui);
+                end;
+                exit Boucle_Principale when PCMC_Stop;
+            end loop Boucle_Principale;
+            New_Line;
+            Put_Line ("==> Programme serveur interrompu " &
+                "Fermeture des connexions ...");
+            Close_Socket (Client);
+            Close_Socket (Socket);
+        exception
+            when PCMC_Network_Exception =>
+                Put_Line ("==> Client déconnecté !");
+                Close_Socket (Client);
+                Close_Socket (Socket);
+        end Recv_Trame;
 
         task Suivre_Commander;
         task body Suivre_Commander is
             periode : constant Time_Span := Ada.Real_Time.Milliseconds (20);
             Echeance : Time;
-            danger : T_Danger;
             centre : Float;
             somme : Float;
             compt : Float;
-            Pui : T_Power;
+            Pui_trame : Pcmc_Header.T_Power;
+            Pui_integer : Integer;
+            Pui : Tk_Periph.Motors.T_Power;
+            Pui_Motor : Tk_Periph.Motors.T_Power;
             Piste : T_Tracking;
         begin
-            Le_Tank.Move (Pow);
             loop
+                Pui_trame := Puissance.Lire;
+                Put_Line ("puissance lue " &
+                    Pcmc_Header.T_Power'Image (Pui_trame));
+                Pui_integer := Pcmc_Header.T_Power'Pos (Pui_trame);
+                Pui_Motor :=
+                    Tk_Periph.Motors.T_Power ((Pui_integer * 100) / 32767);
+                    Put_Line ("Moving with power " &
+                        Tk_Periph.Motors.T_Power'Image (Pui_Motor));
+                Le_Tank.Move (Pui_Motor);
                 compt := 0.0;
                 somme := 0.0;
                 Echeance := Clock + periode;
-                danger := Arret.Lire;
-                if danger = Stop then
-                    exit;
-                elsif danger = Pause then
-                    Le_Tank.Stop;
-                else
-                    Le_Tank.Track (Piste);
-                    for I in T_Index_Sensors loop
-                        if Le_Tank.Get_Track_Channel (Piste, I) = True then
-                            somme := somme + Float (I);
-                            compt := compt + 1.0;
-                        end if;
-                    end loop;
-                    if compt = 0.0 then
-                        Spin (Le_Tank, Pow);
-                    else
-                        centre := somme / compt;
-                        Pui := T_Power ((centre - 1.5) * 6.0 * 0.6);
-                        Le_Tank.Left (Pow - Pui);
-                        Le_Tank.Right (Pow + Pui);
+                Le_Tank.Track (Piste);
+                for I in T_Index_Sensors loop
+                    if Le_Tank.Get_Track_Channel (Piste, I) = True then
+                        somme := somme + Float (I);
+                        compt := compt + 1.0;
                     end if;
+                end loop;
+                if compt = 0.0 then
+                    Spin (Le_Tank, Pui_Motor);
+                    Put_Line ("Stuck at this condition");
+                else
+                    Put_Line ("Calculating the new center of the track");
+                    centre := somme / compt;
+                    Pui := Tk_Periph.Motors.T_Power
+                        ((centre - 1.5) * 6.0 * 0.6);
+                    Le_Tank.Left (Pui_Motor - Pui);
+                    Le_Tank.Right (Pui_Motor + Pui);
                 end if;
                 delay until Echeance;
             end loop;
@@ -102,17 +137,4 @@ begin
     begin
         null;
     end;
-    Boucle_Infinie :
-    loop
-        delay 2.0;
-        Put_Line
-           (Item =>
-               "Dist. obst. = " &
-               T_Distance'Image (Le_Tank.Get_Ultrasonic_Distance));
-        Le_Tank.Track (Piste);
-        for I in T_Index_Sensors loop
-            Put (Item =>
-                   Boolean'Image (Le_Tank.Get_Track_Channel (Piste, I)) & "-");
-        end loop;
-    end loop Boucle_Infinie;
 end Ctrl_Tank;
